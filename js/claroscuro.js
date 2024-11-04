151d27e27c15b71897b8bd66a150e4b5d341354d
PerezGero <geroperezcollazo@gmail.com>
2024-11-04 11:07:43 -0300

    comentariosExtras

--- js/claroscuro.js
+++ js/claroscuro.js
@@ -1,16 +1,16 @@
 function cambiarColor() {
-
+    // anclo a una constante el get element by id de aquellos con id claroscuro
 	const div = document.getElementById("claroscuro");
 
-	// Verifica el color actual del div 
+	// Verifica que el color actual del div sea el gris mas oscuro
     if (div.style.backgroundColor === "rgba(130, 130, 130, 0.8)") {
 
-        // Si es "gis scuro", cambia al color original
+        // Si es gis oscuro, cambia al color original, sea gris claro
 
         div.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
     } else {
 
-        // Si no es "gris oscuro", cambia a este
+        // Si no es gris oscuro, cambia a este mismo
 
         div.style.backgroundColor = "rgba(130, 130, 130, 0.8)";
     }
